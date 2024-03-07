12abf9a5995eef941fff1f2e0ff1a79aed5a7d60
everulicess <ulices.vaca12@gmail.com>
2024-03-07 16:22:48 +0100

    animaitions transitions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,9 @@ public class Player : NetworkBehaviour
     [SerializeField] private Transform camTarget;
 
     float turnSpeed = 360f;
-    float speed = 10f;
+    float speed = 300f;
 
+    [SerializeField] Animator anim;
     public override void Spawned()
     {
         if (HasInputAuthority)
@@ -22,11 +23,13 @@ public class Player : NetworkBehaviour
     private void Awake()
     {
         characterController = GetComponent<NetworkCharacterController>();
+        anim = GetComponent<Animator>();
     }
     public override void FixedUpdateNetwork()
     {
         if (GetInput(out NetworkInputData data))
         {
+            anim.SetBool("Moving", true);
             var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
 
             var skewedInput = matrix.MultiplyPoint3x4(data.direction);
@@ -38,8 +41,14 @@ public class Player : NetworkBehaviour
 
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Runner.DeltaTime);
             }
-            data.direction.Normalize();
-            characterController.Move(speed * skewedInput/*(transform.forward* data.direction.magnitude)*/ * Runner.DeltaTime);
+            //data.direction.Normalize();
+            characterController.Move(Runner.DeltaTime * speed * skewedInput);
+
+            if (data.direction == Vector3.zero)
+                anim.SetBool("Moving", false);
         }
+
+
+
     }
 }
