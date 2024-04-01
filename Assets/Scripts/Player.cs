50a5d8e97533d5293331feaf4f09f9d275709fd7
everulicess <ulices.vaca12@gmail.com>
2024-04-01 22:14:04 +0200

    fixes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -44,29 +44,47 @@ public class Player : NetworkBehaviour {
     }
     public override void FixedUpdateNetwork()
     {
-        characterController.maxSpeed = Character.MovementStats.MovementSpeed;
-        
-        
-        if (GetInput(out NetworkInputData data))
+
+        //count down attack time and end the attack once it hits 0
+        if (IsAttacking && attackTime > 0)
         {
-            anim.SetBool("Moving", true);
-            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
-            //data.direction.Normalize();
-
-            var skewedInput = matrix.MultiplyPoint3x4(data.direction);
-
-            if (data.direction != Vector3.zero)
-            {
-                var relative = (transform.position + data.direction) - transform.position;
-                var rot = Quaternion.LookRotation(relative, Vector3.up);
-
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Runner.DeltaTime);
-            }
-            skewedInput.Normalize();
-            characterController.Move(Runner.DeltaTime * speed * skewedInput);
-            //Debug.LogWarning($"skewed input is {skewedInput}");
-            if (data.direction == Vector3.zero)
-                anim.SetBool("Moving", false);
+            attackTime -= Runner.DeltaTime;
+            if (attackTime <= 0) IsAttacking = false;
         }
+
+        //exit if there is no input
+        if (!GetInput(out NetworkInputData data)) return;
+
+        //initiate attack with mouse click (also checks if the player is already attacking so the animation does not restart)
+        if (Input.GetKey(KeyCode.Mouse0) && !IsAttacking) IsAttacking = true;
+
+        Vector3 forward = Vector3.zero;
+        if (data.direction != Vector3.zero)
+        {
+            forward = skew * data.direction;
+            //rotate the character unless it is currently attacking. The NetworkCharacterController's rotation speed should be 0 so it doesn't interfere with this.
+            if (!IsAttacking) transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(forward, Vector3.up), turnSpeed * Runner.DeltaTime);
+        }
+
+        //movement blending variables
+        if (characterController.Velocity == Vector3.zero)
+        {
+            anim.SetFloat("MoveX", 0);
+            anim.SetFloat("MoveY", 0);
+        }
+        else
+        {
+            //get the angle between the forward and movement vectors and multiply it by 2pi/360 to get the correct sine/cosine
+            float angle = Vector3.SignedAngle(transform.forward, characterController.Velocity, Vector3.up) * 0.0174532925199f;
+            //calculate the ratio between current and maximum speed to make blend walk and idle depending on speed
+            float ratio = characterController.Velocity.magnitude / characterController.maxSpeed;
+            //sine and cosine times ratio gives the final x and y values
+            anim.SetFloat("MoveX", Mathf.Sin(angle) * ratio);
+            anim.SetFloat("MoveY", Mathf.Cos(angle) * ratio);
+        }
+
+        //move the character
+        characterController.Move(forward);
+        anim.SetBool("Moving", characterController.Velocity != Vector3.zero);
     }
 }
