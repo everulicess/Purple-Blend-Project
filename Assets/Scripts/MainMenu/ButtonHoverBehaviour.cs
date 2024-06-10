8040d3143e8795280e78b243d0c44e682e7c0ae0
everulicess <ulices.vaca12@gmail.com>
2024-06-10 20:16:49 +0200

    Update ButtonHoverBehaviour.cs

--- Assets/Scripts/MainMenu/ButtonHoverBehaviour.cs
+++ Assets/Scripts/MainMenu/ButtonHoverBehaviour.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class ButtonHoverBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     RectTransform rectTransform;
     float scale = 1.25f;
-    Vector3 scaleVector = new(1, 1, 1);
+    [SerializeField] Vector3 scaleVector;
     public void OnPointerClick(PointerEventData eventData)
     {
         rectTransform.localScale /= scale;
@@ -20,10 +21,19 @@ public class ButtonHoverBehaviour : MonoBehaviour, IPointerEnterHandler, IPointe
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        rectTransform.localScale = scaleVector;
+        if (SceneManager.GetActiveScene().name == "MenuScene")
+        {
+            rectTransform.localScale = scaleVector;
+        }
+        else
+        {
+            rectTransform.localScale = new(1, 1, 1);
+        }
     }
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        scaleVector = rectTransform.localScale;
+
     }
 }
